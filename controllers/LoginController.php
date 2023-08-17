fd27df00ff1006674d81ae232de8a6d3701350a5
Carlos Adiel Reyes Soto <carlosreyes1803@gmail.com>
2023-08-16 18:28:48 -0600

    Se configuro errores que tenian en el codigo, el cual no permitica verificar los usuarios ni iniciar secion

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -3,45 +3,45 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Usuario;
 
 class LoginController {
-    public static function index(Router $router){
-        $router->render('login/index',[]);
+
+    public static function index(Router $router) {
+        $router->render('login/index', []);
     }
 
-    public static function loginAPI(){
-        
+    public static function loginAPI() {
         $catalogo = filter_var($_POST['usu_catalogo'], FILTER_SANITIZE_NUMBER_INT);
         $password = filter_var($_POST['usu_password'], FILTER_DEFAULT);
         $usuarioRegistrado = Usuario::fetchFirst("SELECT * FROM usuario WHERE usu_catalogo = $catalogo");
 
         try {
-            if(is_array($usuarioRegistrado)){
+            if (is_array($usuarioRegistrado)) {
                 $verificacion = password_verify($password, $usuarioRegistrado['usu_password']);
-                $nombre = $usuarioRegistrado['usu_nombre'];
-                if($verificacion){
+                $nombre = $usuarioRegistrado["usu_nombre"];
+                
+                if ($verificacion) {
                     session_start();
                     $_SESSION['auth_user'] = $catalogo;
 
                     echo json_encode([
                         'codigo' => 1,
-                        'mensaje' => "Sesion iniciada correctamente. Bienvenido $nombre"
+                        'mensaje' => "Sesión iniciada correctamente. Bienvenido $nombre"
                     ]);
-                }else{
-                    echo json_decode([
+                } else {
+                    echo json_encode([
                         'codigo' => 2,
                         'mensaje' => 'Contraseña incorrecta'
                     ]);
                 }
-            }else{
+            } else {
                 echo json_encode([
                     'codigo' => 2,
                     'mensaje' => 'Usuario no encontrado'
                 ]);
-
             }
-
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             echo json_encode([
                 'detalle' => $e->getMessage(),
                 'codigo' => 0,
--- src/js/login/index.js
+++ src/js/login/index.js
@@ -1,42 +1,53 @@
-import { Dropdown } from "bootstrap";
-import Swal from "sweetalert2";
 import { validarFormulario, Toast } from "../funciones";
-
 
 const formLogin = document.querySelector('form');
 
-
 const login = async e => {
     e.preventDefault();
-    if(!validarFormulario(formLogin)){
+
+    if (!validarFormulario(formLogin)) {
         Toast.fire({
             icon: 'info',
-            title: 'Debe llenar Todos los campos'
-        })
-        return
+            title: 'Debe llenar todos los campos'
+        });
+        return;
     }
 
     try {
-        const url = '/login_reyes/API/login'
-        
+        const url = "/login_reyes/API/login"; 
+
         const body = new FormData(formLogin);
 
         const headers = new Headers();
-        headers.append("X-Requested-with", "fetch");
+        headers.append("X-Requested-With", "fetch");
+
         const config = {
             method: 'POST',
             headers,
             body
-        }
+        };
 
         const respuesta = await fetch(url, config);
         const data = await respuesta.json();
-        console.log(data);
 
+        const {codigo, mensaje, detalle} = data;
+        let icon = 'info';
+        if(codigo == 1){
+            icon = 'success'
+        }else if(codigo == 2){
+            icon = 'warning'
+        }else{
+            icon = 'error'
+        }
+
+        Toast.fire({
+            title : mensaje,
+            icon
+        })
+      
     } catch (error) {
         console.log(error);
     }
+}
 
-    }
-
-    formLogin.addEventListener('submit', login);
+formLogin.addEventListener('submit', login);
